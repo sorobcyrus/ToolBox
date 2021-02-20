f74ca0ff3f094c7e19b81c47b3151b619da1cab5
sorobcyrus <70861776+sorobcyrus@users.noreply.github.com>
2021-02-20 11:24:29 -0700

    I

--- AntiJoin.sql
+++ AntiJoin.sql
@@ -4,7 +4,7 @@ File: AntiJoin.sql
 Create Date:    2021-01-01 
 Author:         Sorob Cyrus
 Description:    How to define an antijoin (e.g. Find those customers who have not ordered)
-                Instead of a long Not exists, we can use Expect!
+                Instead of a long Not exists, we can use Except!
 ****************************************************************************************************
 SUMMARY OF CHANGES
 Date(yyyy-mm-dd)    Author              Comments
