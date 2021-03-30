782a2aaaa7a4692ca7bd5857b072da58418467ce
sorobcyrus <70861776+sorobcyrus@users.noreply.github.com>
2021-03-30 10:46:28 -0600

    -

---
+++ FKs_with_sp_fkeys_and_objectid.sql
@@ -0,0 +1,13 @@
+--Find FKs for a table
+EXEC sp_fkeys @pktable_name = 'TblName', @pktable_owner = 'SchemaName'
+
+--This field name is misleading.. It should've been named 
+--relationship_owner_object_id
+SELECT parent_object_id
+    FROM sys.foreign_keys
+    WHERE object_id = OBJECT_ID(N'Person.FK_Name')
+
+--Add FK to an existing Table
+ALTER TABLE [Schema].[FK_TblName]  WITH CHECK ADD  CONSTRAINT [FK_Name] 
+								FOREIGN KEY([FKColumnID])
+								REFERENCES [Schema].[PKTblName] ([PKColumnID])
